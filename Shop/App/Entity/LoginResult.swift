8ee1f280c1138c3cb186884fd1df6044756b23dc
skokdmitriy <84864953+skokdmitriy@users.noreply.github.com>
2023-06-15 23:13:59 +0700

    Server responses

---
+++ Shop/App/Entity/LoginResult.swift
@@ -0,0 +1,13 @@
+//
+//  LoginResult.swift
+//  Shop
+//
+//  Created by Дмитрий Скок on 15.06.2023.
+//
+
+import Foundation
+
+struct LoginResult: Codable {
+    let result: Int
+    let user: User
+}
